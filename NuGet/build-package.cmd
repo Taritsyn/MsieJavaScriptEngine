c4b04d92c8e577ebafef5ebdd97cd7d378e45957
Andrey Taritsyn <taritsyn@gmail.com>
2015-10-17 11:20:17 +0300

    Removed redundant nuget.exe

--- NuGet/build-package.cmd
+++ NuGet/build-package.cmd
@@ -1,2 +1,2 @@
 \Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\MsieJavaScriptEngine\MsieJavaScriptEngine.csproj /p:Configuration=Release
-nuget.exe pack MsieJavaScriptEngine.nuspec
+..\.nuget\nuget.exe pack MsieJavaScriptEngine.nuspec
