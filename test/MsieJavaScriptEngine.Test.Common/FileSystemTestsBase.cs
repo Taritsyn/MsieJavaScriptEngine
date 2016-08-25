a64f7c966fbe99ca6fd096b2bf095a9c1b74e533
Andrey Taritsyn <taritsyn@gmail.com>
2016-08-25 20:41:16 +0300

    Added a version for .NET Framework 4.5.1

--- test/MsieJavaScriptEngine.Test.Common/FileSystemTestsBase.cs
+++ test/MsieJavaScriptEngine.Test.Common/FileSystemTestsBase.cs
@@ -1,21 +1,33 @@
-﻿using System;
-using System.IO;
+﻿#if NET451
+using Microsoft.Extensions.PlatformAbstractions;
+#elif NET40
+using System;
 using System.Text.RegularExpressions;
+#else
+#error No implementation for this target
+#endif
+using System.IO;
 
 namespace MsieJavaScriptEngine.Test.Common
 {
 	public abstract class FileSystemTestsBase
 	{
+#if NET40
 		/// <summary>
 		/// Regular expression for working with the `bin` directory path
 		/// </summary>
 		private readonly Regex _binDirRegex = new Regex(@"\\bin\\(?:Debug|Release)\\?$", RegexOptions.IgnoreCase);
 
+#endif
 		protected string _baseDirectoryPath;
 
 
 		protected FileSystemTestsBase()
 		{
+#if NET451
+			var appEnv = PlatformServices.Default.Application;
+			_baseDirectoryPath = Path.Combine(appEnv.ApplicationBasePath, "../../../../../");
+#elif NET40
 			string baseDirectoryPath = AppDomain.CurrentDomain.BaseDirectory;
 			if (_binDirRegex.IsMatch(baseDirectoryPath))
 			{
@@ -23,6 +35,9 @@ namespace MsieJavaScriptEngine.Test.Common
 			}
 
 			_baseDirectoryPath = baseDirectoryPath;
+#else
+#error No implementation for this target
+#endif
 		}
 	}
 }
