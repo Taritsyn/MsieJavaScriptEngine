cf0d15f889b5c82d300e15f3c97eab5abdcf40ba
Andrey Taritsyn <taritsyn@gmail.com>
2017-10-02 21:56:59 +0300

    Added a unit tests to verify correctness of the error message generation

--- test/MsieJavaScriptEngine.Test.Auto/CommonTests.cs
+++ test/MsieJavaScriptEngine.Test.Auto/CommonTests.cs
@@ -4,10 +4,11 @@ namespace MsieJavaScriptEngine.Test.Auto
 {
 	public class CommonTests : CommonTestsBase
 	{
-		protected override MsieJsEngine CreateJsEngine()
+		protected override MsieJsEngine CreateJsEngine(bool enableDebugging)
 		{
 			var jsEngine = new MsieJsEngine(new JsEngineSettings
 			{
+				EnableDebugging = enableDebugging,
 				EngineMode = JsEngineMode.Auto,
 				UseEcmaScript5Polyfill = false,
 				UseJson2Library = false
--- test/MsieJavaScriptEngine.Test.ChakraEdgeJsRt/CommonTests.cs
+++ test/MsieJavaScriptEngine.Test.ChakraEdgeJsRt/CommonTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+
+using NUnit.Framework;
 
 using MsieJavaScriptEngine.Test.Common;
 
@@ -7,10 +9,11 @@ namespace MsieJavaScriptEngine.Test.ChakraEdgeJsRt
 	[TestFixture]
 	public class CommonTests : CommonTestsBase
 	{
-		protected override MsieJsEngine CreateJsEngine()
+		protected override MsieJsEngine CreateJsEngine(bool enableDebugging)
 		{
 			var jsEngine = new MsieJsEngine(new JsEngineSettings
 			{
+				EnableDebugging = enableDebugging,
 				EngineMode = JsEngineMode.ChakraEdgeJsRt,
 				UseEcmaScript5Polyfill = false,
 				UseJson2Library = false
@@ -18,5 +21,111 @@ namespace MsieJavaScriptEngine.Test.ChakraEdgeJsRt
 
 			return jsEngine;
 		}
+
+		#region Mapping errors
+
+		[Test]
+		public void GenerationOfParseErrorMessageIsCorrect()
+		{
+			// Arrange
+			const string input = @"var arr = [];
+var obj = {};
+var foo = 'Browser's bar';";
+			string targetOutput = "Compile error: Expected ';'" + Environment.NewLine +
+				"   at 3:20"
+				;
+
+			JsException exception = null;
+
+			// Act
+			using (var jsEngine = CreateJsEngine())
+			{
+				try
+				{
+					jsEngine.Execute(input, "variables.js");
+				}
+				catch (JsRuntimeException e)
+				{
+					exception = e;
+				}
+			}
+
+			Assert.NotNull(exception);
+			Assert.IsNotEmpty(exception.Message);
+			Assert.AreEqual(targetOutput, exception.Message);
+		}
+
+		[Test]
+		public void GenerationOfParseErrorMessageInDebugModeIsCorrect()
+		{
+			// Arrange
+			const string input = @"var arr = [];
+var obj = {};
+var foo = 'Browser's bar';";
+			string targetOutput = "Compile error: Expected ';'" + Environment.NewLine +
+				"   at 3:20"
+				;
+
+			JsException exception = null;
+
+			// Act
+			using (var jsEngine = CreateJsEngine(true))
+			{
+				try
+				{
+					jsEngine.Execute(input, "variables.js");
+				}
+				catch (JsRuntimeException e)
+				{
+					exception = e;
+				}
+			}
+
+			Assert.NotNull(exception);
+			Assert.IsNotEmpty(exception.Message);
+			Assert.AreEqual(targetOutput, exception.Message);
+		}
+
+		[Test]
+		public void GenerationOfRuntimeErrorMessageIsCorrect()
+		{
+			// Arrange
+			const string input = @"function foo(x, y) {
+	var z = x + y;
+	if (z > 20) {
+		bar();
+	}
+}
+
+var a = 8;
+var b = 15;
+
+foo(a, b);";
+			string targetOutput = "ReferenceError: 'bar' is undefined\n" +
+				"   at foo (functions.js:4:3)\n" +
+				"   at Global code (functions.js:11:1)"
+				;
+
+			JsException exception = null;
+
+			// Act
+			using (var jsEngine = CreateJsEngine())
+			{
+				try
+				{
+					jsEngine.Execute(input, "functions.js");
+				}
+				catch (JsRuntimeException e)
+				{
+					exception = e;
+				}
+			}
+
+			Assert.NotNull(exception);
+			Assert.IsNotEmpty(exception.Message);
+			Assert.AreEqual(targetOutput, exception.Message);
+		}
+
+		#endregion
 	}
 }
