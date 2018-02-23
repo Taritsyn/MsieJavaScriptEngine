a85276a8fe0a1932003a02d4d3ea48406fdb4940
Andrey Taritsyn <taritsyn@gmail.com>
2018-02-23 14:49:27 +0300

    Changed a `build.ps1` and `build.sh` files

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ if test ! -d $packageDir/NUnit.Runners; then
   mono $localNugetPackageManager install NUnit.Runners -Version 3.4.1 -O $packageDir% -ExcludeVersion -NoCache
 fi
 
-koreBuildZip="https://github.com/aspnet/KoreBuild/archive/02bd945d32558d24c1e5c6b74e37d44585ad9691.zip"
+koreBuildZip="https://codeload.github.com/aspnet/KoreBuild/zip/02bd945d32558d24c1e5c6b74e37d44585ad9691"
 if [ ! -z $KOREBUILD_ZIP ]; then
     koreBuildZip=$KOREBUILD_ZIP
 fi
