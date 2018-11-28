1b54aeb9a58bbe8833acae507bd9dfd560d47e17
Andrey Taritsyn <taritsyn@gmail.com>
2018-11-28 16:39:46 +0300

    Added a tests for variables and functions with names containing Unicode characters

--- test/MsieJavaScriptEngine.Test.Common/CommonTestsBase.cs
+++ test/MsieJavaScriptEngine.Test.Common/CommonTestsBase.cs
@@ -638,6 +638,29 @@ namespace MsieJavaScriptEngine.Test.Common
 			Assert.AreEqual("Привет, Петя!", output);
 		}
 
+		[Test]
+		public virtual void CallingOfFunctionWithNameContainingUnicodeCharactersIsCorrect()
+		{
+			// Arrange
+			const string functionCode = @"function сумма(число1, число2) {
+	var результат = число1 + число2;
+
+	return результат;
+}";
+
+			// Act
+			int output;
+
+			using (var jsEngine = CreateJsEngine())
+			{
+				jsEngine.Execute(functionCode);
+				output = jsEngine.CallFunction<int>("сумма", 678, 711);
+			}
+
+			// Assert
+			Assert.AreEqual(1389, output);
+		}
+
 		#endregion
 
 		#region Getting, setting and removing variables
@@ -898,6 +921,30 @@ namespace MsieJavaScriptEngine.Test.Common
 			// Arrange
 			const string variableName = "price";
 			const double input = 120.55;
+
+			// Act
+			bool variableBeforeRemovingExists;
+			bool variableAfterRemovingExists;
+
+			using (var jsEngine = CreateJsEngine())
+			{
+				jsEngine.SetVariableValue(variableName, input);
+				variableBeforeRemovingExists = jsEngine.HasVariable(variableName);
+				jsEngine.RemoveVariable(variableName);
+				variableAfterRemovingExists = jsEngine.HasVariable(variableName);
+			}
+
+			// Assert
+			Assert.IsTrue(variableBeforeRemovingExists);
+			Assert.IsFalse(variableAfterRemovingExists);
+		}
+
+		[Test]
+		public virtual void RemovingVariableWithNameContainingUnicodeCharactersIsCorrect()
+		{
+			// Arrange
+			const string variableName = "цена";
+			const double input = 6780.00;
 
 			// Act
 			bool variableBeforeRemovingExists;
